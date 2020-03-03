1ef67b95ec19031e8874050cf9d823312bb2fcb0
grosse1 <emilygross5144@gmail.com>
2020-03-03 14:41:27 -0700

    Added weapon choice

--- CS125Project.c
+++ CS125Project.c
@@ -13,7 +13,38 @@ void sleep()
 
 void investigate()
 {
-	
+	char choice;
+	int item;
+	printf("Before you leave, would you like to take an item?(Y/N)");
+	scanf(" %c", &choice);
+	if(choice=='Y' || choice=='y' || choice=='N' || choice=='n')
+	{
+		do
+		{
+			printf("What item would you like to take?\n1-Pocket knife.\n2-Baseball bat.\n3-Nevermind.");
+			scanf("%d", &item);
+			switch(item)
+			{
+				case 1:
+					printf("You grab your trusty pocket knife.\n");
+					break;
+				case 2:
+					printf("You grab your trusty baseball bat.\n");
+					break;
+				case 3:
+					printf("You leave your room empty-handed.\n");
+					break;
+				default:
+					printf("Invalid choice. Try again.\n");
+					break;
+			}
+		}
+		while(item<1 || item>3);
+	}
+	else
+	{
+		item=0;
+	}
 }
 
 void listen()
@@ -27,7 +58,7 @@ int main()
 	do
 	{
 		printf("You're sleeping soundly when suddenly you jolt awake.\nYou check your alarm clock: 2:37 AM.\nWhile wondering about what could have woken you, you hear a thud from downstairs.\nWhat do you do?\n");
-		printf("1-Go back to sleep.\n2-Go downstairs and investigate.\n3-Wait and listen.\n");
+		printf("1-Go back to sleep.\n2-Go downstairs and investigate.\n3-Wait and listen some more.\n");
 		scanf("%d", &x);
 		switch(x)
 		{
