d258d64748bd29d66287662cb3cafa5655375667
grosse1 <emilygross5144@gmail.com>
2020-03-03 13:58:34 -0700

    debugged first branch

--- CS125Project.c
+++ CS125Project.c
@@ -24,6 +24,7 @@ int main()
 {
 	int x;
 	do
+	{
 		printf("You're sleeping soundly when suddenly you jolt awake.\nYou check your alarm clock: 2:37 AM.\nWhile wondering about what could have woken you, you hear a thud from downstairs.\nWhat do you do?\n");
 		printf("1-Go back to sleep.\n2-Go downstairs and investigate.\n3-Wait and listen.\n");
 		scanf("%d", &x);
@@ -39,9 +40,10 @@ int main()
 				listen();
 				break;
 			default:
-				printf("Invalid choice.\n");
+				printf("Invalid choice. Try again.\n");
 				break;
 		}
-	while(x!=1 || x!=2 || x!=3)
+	}
+	while(x<1 || x>3);
     return 0;
 }
