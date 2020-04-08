6c5abe2e3523dcb0c7c3e014e3e304a5bd9b1b90
Codyyyyyyyy <61477690+Codyyyyyyyy@users.noreply.github.com>
2020-04-08 11:31:08 -0700

    Added files
    main.c --contains the main function and calls the clearScreen() function
    
    listenBranch.c --contains listenBranch functions as well as the clearScreen() and getItem() function
    
    functions.h --contains the function prototypes for all the functions in listenBranch.c

---
+++ functions.h
@@ -0,0 +1,14 @@
+void clearScreen();
+int getItem();
+void petDog(int);
+void fightDog(int);
+void runToFrontDoor(int);
+void attackTheKiller(int);
+void throwSpice(int);
+void hideInCabinet(int);
+void walkAwayFromKiller(int);
+void waitForKillerToLeave();
+void hideUnderBed();
+void hideInCloset();
+void listen();
+
